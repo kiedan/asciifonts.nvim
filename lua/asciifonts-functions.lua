a1179e3375da85397beacf314398ae5bbfa2f6b1
kiedan <net@kiedan.com>
2023-11-26 10:48:53 +0100

    perform figlet command on current line with font selected via telescope

--- lua/asciifonts-functions.lua
+++ lua/asciifonts-functions.lua
@@ -5,15 +5,16 @@ function M.get_font_name()
   local opts = {
     prompt_title= "~ FONT SELECTION ~",
     shorten_path = false,
-    cwd = "~/Development/Plugins/asciifonts.nvim/font-previews",
+    cwd = "~/Dev/plugins/asciifonts.nvim/font-previews",
     attach_mappings = function(_, map)
       map("i", "<CR>", function(prompt_bufnr)
+
         -- filename is available at entry[1]
         local entry = require("telescope.actions.state").get_selected_entry()
         require("telescope.actions").close(prompt_bufnr)
         local filename = entry[1]
-        -- Insert filename in current cursor position
-        vim.cmd('normal i' .. filename)
+		local cmd = '.!figlet -f ' .. filename
+		vim.cmd(cmd)
       end
       )
       return true
--- lua/asciifonts.lua
+++ lua/asciifonts.lua
@@ -1,2 +1,3 @@
 print('init')
-vim.api.nvim_set_keymap('n', '<Leader>ö', [[<Cmd>lua require('asciifonts-functions').get_font_name()<CR>]], {})
+vim.api.nvim_set_keymap('n', '<Leader>aa', [[<Cmd>lua require('asciifonts-functions').get_font_name()<CR>]], {})
+
